a43afafc04ecf2f78995d3203ecb8bf4c1208662
amandaheideman <hedieman.amanda@gmail.com>
2021-05-14 19:18:47 +0200

    update front page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import helloPic from '../images/hello.png'
 
 export default function HomePage() {
     return (
         <div>
-            <h1 className="heading">
-                Home 
-            </h1>
+            <div className="heading">
+                <img src={helloPic} alt="hello" class="hello"></img>
+                <h3>My name is Amanda, and I'm a full stack web developer</h3>
+            </div>
         </div>
     )
 }
